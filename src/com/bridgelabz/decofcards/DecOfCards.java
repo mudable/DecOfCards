e1d65f19b4b8aa8b75b4089eed54d2b76e14dbf5
mudable <mudableshilpa@gmail.com>
2021-11-13 18:23:52 +0530

    Added : UC1_InitializeCards

---
+++ src/com/bridgelabz/decofcards/DecOfCards.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.decofcards;
+
+public class DecOfCards {
+	public static void main(String[] args) {
+		System.out.println("Welcome into the Decks of Cards!!! Thank you");
+		CardsPlay cardsPlay = new CardsPlay();
+		cardsPlay.generateUniqueCards();
+	}
+}
