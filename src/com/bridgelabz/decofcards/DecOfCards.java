957d4e60cca4bf3f268853c4593464092b791450
mudable <mudableshilpa@gmail.com>
2021-11-13 19:24:54 +0530

    Added : UC3 to sequence the player order

--- src/com/bridgelabz/decofcards/DecOfCards.java
+++ src/com/bridgelabz/decofcards/DecOfCards.java
@@ -1,10 +1,18 @@
 package com.bridgelabz.decofcards;
 
+import java.util.Scanner;
+
 public class DecOfCards {
 	public static void main(String[] args) {
-		System.out.println("Welcome into the Decks of Cards!!! Thank you");
-		CardsPlay cardsPlay = new CardsPlay();
-		cardsPlay.generateUniqueCards();
-		cardsPlay.addPlayer();
+		Scanner sc = new Scanner(System.in);
+		System.out.print(" Enter number of players :");
+		int playerCount = sc.nextInt();
+
+		CardsPlay play = new CardsPlay(playerCount);
+		play.generateCards();
+		play.printCards();
+		play.addPlayer();
+		play.distributeCards();
+		play.printCardList();
 	}
 }
--- src/com/bridgelabz/decofcards/Player.java
+++ src/com/bridgelabz/decofcards/Player.java
@@ -1,9 +1,23 @@
 package com.bridgelabz.decofcards;
 
-public class Player {
-	String firstname;
+import java.util.ArrayList;
 
-	public Player(String firstname) {
-		this.firstname = firstname;
+public class Player {
+	String name;
+	ArrayList<String> cardList = new ArrayList<>();
+
+	public Player(String name) {
+		this.name = name;
+	}
+
+	public void setCardList(String card) {
+		cardList.add(card);
+	}
+
+	public void getCardList() {
+		// get all cards inside cardList
+		for (String position : cardList) {
+			System.out.print(position + ", ");
+		}
 	}
 }
