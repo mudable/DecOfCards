34058f67ffd625b121544965659ef5e60bf443a4
mudable <mudableshilpa@gmail.com>
2021-11-13 19:54:18 +0530

    Added : UC4 to Shuffle the cards

--- src/com/bridgelabz/decofcards/CardsPlay.java
+++ src/com/bridgelabz/decofcards/CardsPlay.java
@@ -17,25 +17,30 @@ public class CardsPlay {
 	}
 
 	public void generateCards() {
+
 		String[] suit = { "Clubs", "Diamonds", "Hearts", "Spades" };
 		String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
 		// get combination
 		for (int i = 0; i < suit.length; i++) {
 			for (int j = 0; j < rank.length; j++) {
+
 				cardsArr.add(suit[i] + rank[j]);
 			}
 		}
 	}
 
 	public void printCards() {
-		System.out.print("Cards : ");
+
+		System.out.println("Cards ");
 		for (int i = 0; i < cardsArr.size(); i++) {
-			System.out.print(" " + cardsArr.get(i) + "\n");
+			System.out.print(" " + cardsArr.get(i) + " ");
 		}
 	}
 
 	public void addPlayer() {
+
 		Scanner sc = new Scanner(System.in);
+
 		if (playerCount >= 2 && playerCount <= 4) {
 			for (int i = 1; i <= playerCount; i++) {
 				System.out.print(" Enter player " + i + " name : ");
@@ -46,8 +51,26 @@ public class CardsPlay {
 		}
 	}
 
+	public void shuffleCards() {
+		for (int i = 0; i < cardsArr.size(); i++) {
+			int min = 0;
+			int max = cardsArr.size() - 1;
+			int position = (int) (Math.random() * (max - min + 1) + min);
+			int newPosition = (position - i);
+			if (newPosition < 0 || newPosition > 52)
+				newPosition = 0;
+			String temp = cardsArr.get(newPosition);
+			cardsArr.set(newPosition, cardsArr.get(i));
+			cardsArr.set(i, temp);
+		}
+	}
+
 	public String getCards() {
-		int position = (int) (Math.random());
+		int min = 0;
+		int max = cardsArr.size() - 1;
+		int position = (int) (Math.random() * (max - min + 1) + min);
+		System.out.print("\n Size : " + cardsArr.size() + ", position : " + position);
+
 		String returnValue = cardsArr.get(position);
 		cardsArr.remove(position);
 		return returnValue;
@@ -58,13 +81,15 @@ public class CardsPlay {
 			for (Player playerObj : playerList) {
 				// adding each card in cardList of each player
 				playerObj.setCardList(getCards());
-
+				shuffleCards();
 			}
 		}
 	}
 
 	public void printCardList() {
+
 		for (Player playerObj : playerList) {
+
 			System.out.print("\ncards of " + playerObj.name + " : ");
 			playerObj.getCardList();
 			System.out.print("\n ");
--- src/com/bridgelabz/decofcards/DecOfCards.java
+++ src/com/bridgelabz/decofcards/DecOfCards.java
@@ -4,15 +4,13 @@ import java.util.Scanner;
 
 public class DecOfCards {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print(" Enter number of players :");
-		int playerCount = sc.nextInt();
-
-		CardsPlay play = new CardsPlay(playerCount);
-		play.generateCards();
-		play.printCards();
-		play.addPlayer();
-		play.distributeCards();
-		play.printCardList();
-	}
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" Enter number of players :");
+        int playerCount = sc.nextInt();
+        CardsPlay play = new CardsPlay(playerCount);
+        play.generateCards();
+        play.addPlayer();
+        play.distributeCards();
+        play.printCardList();
+    }
 }
